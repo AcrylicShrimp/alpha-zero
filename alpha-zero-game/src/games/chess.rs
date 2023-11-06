0610d020aefcf5682406495189f70948be2bfb21
AcrylicShrimp <led789zxpp@naver.com>
2023-11-07 03:00:46 +0900

    fix: return none for illegal moves.

--- alpha-zero-game/src/games/chess.rs
+++ alpha-zero-game/src/games/chess.rs
@@ -534,6 +534,10 @@ impl Game for Chess {
         let board = self.chess.current_position();
         let chess_move = Self::index_to_move(&board, action)?;
 
+        if !board.legal(chess_move) {
+            return None;
+        }
+
         self.chess.make_move(chess_move);
 
         match self.chess.result() {
